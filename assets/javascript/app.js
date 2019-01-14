c0c164b4b85d50c00b28f65dd78f9583f77bef3d
Daniel Barkley <danielbarkley@daniels-mbp.attlocal.net>
2019-01-14 06:40:28 -0600

    Basic HTML and CSS, very little JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,97 @@
+
+
+
+var myQuestions = [
+	{
+		question: "What nickname does Peter give himself?",
+		answers: {
+			a: 'Badass Pete',
+			b: 'Peter the Great',
+            c: 'Pistol Pete',
+            d: 'Star Lord'
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "In the film, Peter Quill was raised by which group of thieves and smugglers?",
+		answers: {
+			a: 'The Ravagers',
+			b: 'The Justice League',
+            c: 'The Avengers',
+            d: 'None of them'
+		},
+		correctAnswer: 'a'
+    },
+    {
+		question: "Which prison are the gang taken to?",
+		answers: {
+			a: 'Star Prison',
+			b: 'Big House',
+            c: 'The Kyln',
+            d: 'The Daeth Star'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "Which planet does Ronan The Accuser attack at the end of the first movie",
+		answers: {
+			a: 'Jupiter',
+			b: 'Alderaan',
+            c: 'Endor',
+            d: 'Xandar'
+		},
+		correctAnswer: 'd'
+	},	{
+		question: "Groot is a Dutch word, meaning...",
+		answers: {
+			a: 'Tree',
+			b: 'Small',
+            c: 'Big',
+            d: 'Wood'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "Who in the mining colony, do they bring the orb to?",
+		answers: {
+			a: 'The Collector',
+			b: 'The Banker',
+            c: 'The Punisher',
+            d: 'The Joker'
+		},
+		correctAnswer: 'a'
+    },
+    {
+		question: "What is the name of Ronan's large ship ?",
+		answers: {
+			a: 'The Pearl',
+			b: 'The Death Aster',
+            c: 'The Dark Aster',
+            d: 'The Falcon'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "After a medical scan, what does the Nova Prime tell Peter about himself?",
+		answers: {
+			a: 'The infinity stone has become a part of him',
+			b: "He's only half human",
+            c: 'He has two hearts',
+            d: "He's related to Gamora"
+		},
+		correctAnswer: 'b'
+	}
+];
+
+$(".btn").click(function () {
+    $(".main-div").hide();
+    startGame();
+});
+
+function startGame () {
+
+}
+
+
+
+
