88ad2cc88e2c7173cf8491e893a7bd2ce85928df
Daniel Barkley <danielbarkley@Daniels-MacBook-Pro.local>
2019-01-15 18:30:30 -0600

    Added HTML, CSS, started on app.js.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 
-
+var quizContainer = document.getElementById("questions");
 
 var myQuestions = [
 	{
@@ -7,8 +7,8 @@ var myQuestions = [
 		answers: {
 			a: 'Badass Pete',
 			b: 'Peter the Great',
-            c: 'Pistol Pete',
-            d: 'Star Lord'
+			c: 'Pistol Pete',
+			d: 'Star Lord'
 		},
 		correctAnswer: 'd'
 	},
@@ -17,8 +17,8 @@ var myQuestions = [
 		answers: {
 			a: 'The Ravagers',
 			b: 'The Justice League',
-            c: 'The Avengers',
-            d: 'None of them'
+			c: 'The Avengers',
+			d: 'None of them'
 		},
 		correctAnswer: 'a'
     },
@@ -27,18 +27,18 @@ var myQuestions = [
 		answers: {
 			a: 'Star Prison',
 			b: 'Big House',
-            c: 'The Kyln',
-            d: 'The Daeth Star'
+			c: 'The Kyln',
+			d: 'The Daeth Star'
 		},
 		correctAnswer: 'c'
     },
     {
-		question: "Which planet does Ronan The Accuser attack at the end of the first movie",
+		question: "Which planet does Ronan The Accuser attack at the end of the first movie?",
 		answers: {
 			a: 'Jupiter',
 			b: 'Alderaan',
-            c: 'Endor',
-            d: 'Xandar'
+			c: 'Endor',
+			d: 'Xandar'
 		},
 		correctAnswer: 'd'
 	},	{
@@ -46,8 +46,8 @@ var myQuestions = [
 		answers: {
 			a: 'Tree',
 			b: 'Small',
-            c: 'Big',
-            d: 'Wood'
+			c: 'Big',
+			d: 'Wood'
 		},
 		correctAnswer: 'c'
     },
@@ -56,18 +56,18 @@ var myQuestions = [
 		answers: {
 			a: 'The Collector',
 			b: 'The Banker',
-            c: 'The Punisher',
-            d: 'The Joker'
+			c: 'The Punisher',
+			d: 'The Joker'
 		},
 		correctAnswer: 'a'
     },
     {
-		question: "What is the name of Ronan's large ship ?",
+		question: "What is the name of Ronan's large ship?",
 		answers: {
 			a: 'The Pearl',
 			b: 'The Death Aster',
-            c: 'The Dark Aster',
-            d: 'The Falcon'
+			c: 'The Dark Aster',
+			d: 'The Falcon'
 		},
 		correctAnswer: 'c'
     },
@@ -76,21 +76,58 @@ var myQuestions = [
 		answers: {
 			a: 'The infinity stone has become a part of him',
 			b: "He's only half human",
-            c: 'He has two hearts',
-            d: "He's related to Gamora"
+			c: 'He has two hearts',
+			d: "He's related to Gamora"
 		},
 		correctAnswer: 'b'
 	}
 ];
 
 $(".btn").click(function () {
-    $(".main-div").hide();
-    startGame();
+		$(".main-div").hide();
+		$()
+		startGame(myQuestions, quizContainer);
 });
 
-function startGame () {
 
+
+function startGame (myQuestions, quizContainer) {
+	// we'll need a place to store the output and the answer choices
+	var output = [];
+	var answers;
+
+	// for each question...
+	for(var i=0; i < myQuestions.length; i++){
+		
+		// first reset the list of answers
+		answers = [];
+
+		// for each available answer...
+		for(letter in myQuestions[i].answers){
+
+			// ...add an html radio button
+			answers.push( 
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ myQuestions[i].answers[letter]
+				+'</label>' + "<br>" 
+			);
+		}
+
+		// add this question and its answers to the output
+		output.push(
+			"<hr>" +
+			'<div class="question">' + myQuestions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
 }
+
+
 
 
 
