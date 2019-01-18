018172120f467606a11ed497197aca4832574556
Daniel Barkley <danielbarkley@daniels-mbp.attlocal.net>
2019-01-18 09:12:29 -0600

    Cleaned up HTML, and CSS. Fixed a few bugs on App.js. Still need to clean up App.js and add a few buttons.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
-
+// For generating the questions
 var quizContainer = document.getElementById("questions");
-var resultsContainer = document.getElementById("results");
-
+// Array of my questions
 var myQuestions = [
 	{
 		question: "What nickname does Peter give himself?",
-		answers: {
+		possibleAnswers: {
 			a: 'Badass Pete',
 			b: 'Peter the Great',
 			c: 'Pistol Pete',
@@ -14,7 +13,7 @@ var myQuestions = [
 	},
 	{
 		question: "In the film, Peter Quill was raised by which group of thieves and smugglers?",
-		answers: {
+		possibleAnswers: {
 			a: 'The Ravagers',
 			b: 'The Justice League',
 			c: 'The Avengers',
@@ -23,7 +22,7 @@ var myQuestions = [
     },
     {
 		question: "Which prison are the gang taken to?",
-		answers: {
+		possibleAnswers: {
 			a: 'Star Prison',
 			b: 'Big House',
 			c: 'The Kyln',
@@ -32,15 +31,16 @@ var myQuestions = [
     },
     {
 		question: "Which planet does Ronan The Accuser attack at the end of the first movie?",
-		answers: {
+		possibleAnswers: {
 			a: 'Jupiter',
 			b: 'Alderaan',
 			c: 'Endor',
 			d: 'Xandar'
 		},
-	},	{
+	},	
+	{
 		question: "Groot is a Dutch word, meaning...",
-		answers: {
+		possibleAnswers: {
 			a: 'Tree',
 			b: 'Small',
 			c: 'Big',
@@ -49,7 +49,7 @@ var myQuestions = [
     },
     {
 		question: "Who in the mining colony, do they bring the orb to?",
-		answers: {
+		possibleAnswers: {
 			a: 'The Collector',
 			b: 'The Banker',
 			c: 'The Punisher',
@@ -58,7 +58,7 @@ var myQuestions = [
     },
     {
 		question: "What is the name of Ronan's large ship?",
-		answers: {
+		possibleAnswers: {
 			a: 'The Pearl',
 			b: 'The Death Aster',
 			c: 'The Dark Aster',
@@ -67,7 +67,7 @@ var myQuestions = [
     },
     {
 		question: "After a medical scan, what does the Nova Prime tell Peter about himself?",
-		answers: {
+		possibleAnswers: {
 			a: 'The infinity stone has become a part of him',
 			b: "He's only half human",
 			c: 'He has two hearts',
@@ -76,20 +76,14 @@ var myQuestions = [
 	}
 ];
 
-$(".btn").click(function () {
-		$(".main-div").hide();
-		startGame(myQuestions, quizContainer);
-		run();
-});
 // Timer functions ===================================================================
-var timer = 20;
+var timer = 1000;
 var intervalId;
 
-function run() {
+function runTimer() {
 	clearInterval(intervalId);
 	intervalId = setInterval(decrement, 1000);
-}
-	
+}	
 function decrement() {
 	timer--;
 	//  Show the number
@@ -98,7 +92,6 @@ function decrement() {
 			stop();
 		}
 }
-	
 function stop() {
 	clearInterval(intervalId);
 		$("#show-timer").hide();
@@ -106,80 +99,102 @@ function stop() {
 	showResults();
 }
 
-// Main game function.
-function startGame () {	
 // Generate questions ================================================================
-
+function startGame () {	
 	// Store output and the answer choices
 	var output = [];
-	var answers;
+	var possibleAnswers;
 
 	// for each question...
 	for(var i=0; i < myQuestions.length; i++){
 		
-		// first reset the list of answers
-		answers = [];
+		// reset the list of possibleAnswers
+		possibleAnswers = [];
 
 		// for each available answer...
-		for(letter in myQuestions[i].answers){
+		for(letter in myQuestions[i].possibleAnswers){
 
 			// add an html radio button
-			answers.push( 
+			possibleAnswers.push( 
 				'<label>'
 					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
 					+ letter + ': '
-					+ myQuestions[i].answers[letter]
+					+ myQuestions[i].possibleAnswers[letter]
 				+'</label>' + "<br>" 
 			);
 		}
 
-		// add this question and its answers to the output
+		// add this question and its possibleAnswers to the output
 		output.push(
 			"<hr>" +
 			'<div class="question">' + myQuestions[i].question + '</div>'
-			+ '<div class="answers">' + answers.join('') + '</div>'
+			+ '<div class="possibleAnswers">' + possibleAnswers.join('') + '</div>' 
+			
 		);
 	}
-	// finally combine our output list into one string of html and put it on the page
+	// combine our output list into one string of html and put it on the page
 	quizContainer.innerHTML = output.join('');
-}
+	// Add 'done' button
+	var btn = $('<button type="button">Done</button>').addClass("btn btn-info btn-lg done");
+	$("#questions").append(btn);
+};
 
 
 // Show results function =============================================================
 
 function showResults () {
-		// gather answer containers from our quiz
-		//$("#results").append("<h3>Incorrect answers: 0</h3>");
-		//var answerContainers = myQuestions.answers;
-		var numCorrect = 0;
-		var numIncorrect = 0;
-		var numUnanswered = 0;
+	// Remove hidden class to show 'results'
+	$("#results").removeClass("hidden");
+	// Set all scores to 0
+	var numCorrect = 0;
+	var numIncorrect = 0;
+	var numUnanswered = 0;
+	// Correct answers
+	var answers = ["d", "a", "c", "d", "c", "a", "c", "b"]
+	// I know that this is too wet. I couldn't get the dry way to work, ran out of time so I did what works.
+	var userpossibleAnswers = [
+	$("input[name=question0]:checked").val(),
+	$("input[name=question1]:checked").val(),
+	$("input[name=question2]:checked").val(),
+	$("input[name=question3]:checked").val(),
+	$("input[name=question4]:checked").val(),
+	$("input[name=question5]:checked").val(),
+	$("input[name=question6]:checked").val(),
+	$("input[name=question7]:checked").val(),
+	];
 
-		var total = 8;
-		var userAnswers = [
-		$("input[name=question0]:checked").val(),
-		$("input[name=question1]:checked").val(),
-		$("input[name=question2]:checked").val(),
-		$("input[name=question3]:checked").val(),
-		$("input[name=question4]:checked").val(),
-		$("input[name=question5]:checked").val(),
-		$("input[name=question6]:checked").val(),
-		$("input[name=question7]:checked").val(),
-		];
-		
-		console.log(userAnswers);
+	for(var i = 0; i < 8; i++) {
 
-		var answers = ["d", "a", "c", "d", "c", "a", "c", "b"]
-		console.log(answers);
-		for(var i = 0; i < total; i++) {
+		if (userpossibleAnswers[i] === answers[i]) {
+			numCorrect++;
+			$("#correct").html("<h3>Correct possibleAnswers: "+numCorrect+"</h3>");
+		} 		else if (userpossibleAnswers[i] === undefined){
+			  		numUnanswered++;
+					$("#unanswered").html("<h3>Unanswered: "+numUnanswered+"</h3>");
+				}		else if (userpossibleAnswers[i] !== answers[i]) {
+							numIncorrect++;
+							$("#incorrect").html("<h3>Incorrect possibleAnswers: "+numIncorrect+"</h3>");
+						};
+	};
+};
 
-			if (userAnswers[i] === answers[i]) {
-				numCorrect++;
-				$("#results").html("<h3>Correct answers: "+numCorrect+"</h3>");
-			}/*else if(userAnswers[i] !== answers[i]) {
-					numIncorrect++;
-					$("#results").html("<h3>Correct answers: "+numIncorrect+"</h3>");
-				}*/
-			}
+// Click events =====================================================================
 
-		}
+$(".start-game").click(function () {
+	$(".main-div").hide();
+	$("#questions").show();
+	runTimer();
+	startGame();
+});
+
+$(document.body).on("click", ".done", function () {
+	$("#show-timer").hide();
+	$("#questions").hide();
+	showResults();
+})
+
+//$(".try-again").click(function () {
+//$("#results").hide();
+//startGame();
+//runTimer();
+//});
